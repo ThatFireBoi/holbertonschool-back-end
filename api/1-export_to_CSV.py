264f523da15426333929618175d1aa56e2fd710f
ThatFireBoi <gcf2007@hotmail.com>
2023-12-19 09:58:15 -0400

    Extends Python script to export data in the CSV format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    name = response.json().get("username")
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        argv[1])
+    response = requests.get(url)
+    tasks = response.json()
+    with open("{}.csv".format(argv[1]), "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([argv[1], name, task.get("completed"),
+                             task.get("title")])
